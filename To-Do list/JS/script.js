c8470711b46bc6cf12aee055f915abda85a1baf9
Mariam <mariamm0hammed91004@gmail.com>
2025-07-27 03:02:40 +0300

    second commit

--- To-Do list/JS/script.js
+++ To-Do list/JS/script.js
@@ -2,6 +2,9 @@ let todoForm = document.getElementById("todo-form");
 let todoInput = document.getElementById("todo-input");
 let todoList = document.getElementById("todo-list");
 
+todoList.innerHTML = localStorage.getItem("todoList");
+addListener();
+
 todoForm.addEventListener("submit", function (e) {
     e.preventDefault();
     let text = todoInput.value.trim();
@@ -15,16 +18,35 @@ function addTask(text) {
     let li = document.createElement("li");
     li.innerHTML = `
     <span>${text}</span>
-    <button class="delete-btn" id="btn">Remove</button>
-    <button class="done-btn" id="btn">Done</button>`;
+    <button class="delete-btn">Remove</button>
+    <button class="done-btn">Done</button>`;
 
     li.querySelector(".delete-btn").addEventListener("click", function () {
         li.remove();
+        localStorage.setItem("todoList", todoList.innerHTML);
     });
 
     li.querySelector(".done-btn").addEventListener("click", function () {
         li.classList.toggle("done");
+        localStorage.setItem("todoList", todoList.innerHTML);
+
     });
 
     todoList.appendChild(li);
+    localStorage.setItem("todoList", todoList.innerHTML);
+
+}
+
+function addListener() {
+    let items = todoList.querySelectorAll("li");
+    items.forEach(li => {
+        li.querySelector(".delete-btn")?.addEventListener("click", function () {
+            li.remove();
+            localStorage.setItem("todoList", todoList.innerHTML);
+        });
+        li.querySelector(".done-btn").addEventListener("click", function () {
+            li.classList.toggle("done");
+            localStorage.setItem("todoList", todoList.innerHTML);
+        });
+    });
 }
